0eee4d2a5016e450f99cf9fc41ec791c137da0e4
jameshu <james.hu@barecovetech.com>
2024-01-24 17:08:30 +0800

    feat: add vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,51 @@
 import './App.css';
 import { Home } from './pages/home';
 import { Analytics } from '@vercel/analytics/react';
+import { ConfigProvider, Layout } from 'antd';
+import { Adsense } from '@ctrl/react-adsense';
+const { Header, Footer, Content } = Layout;
 function App() {
     return (
         <>
-            <div className={'flex justify-center items-center w-full h-full'}>
-                <Home></Home>
-            </div>
+            <ConfigProvider
+                theme={{
+                    components: {
+                        Layout: {
+                            headerBg: '#f5f5f5',
+                        },
+                    },
+                }}
+            >
+                <Layout className={'h-full w-full'}>
+                    <Header>
+                        <Adsense
+                            client="ca-pub-4882664754361963"
+                            slot="8810644722"
+                            style={{ display: 'block' }}
+                            layout="in-article"
+                            format="fluid"
+                        />
+                    </Header>
+                    <Content>
+                        <div
+                            className={
+                                'flex justify-center items-center w-full h-full'
+                            }
+                        >
+                            <Home></Home>
+                        </div>
+                    </Content>
+                    <Footer>
+                        <Adsense
+                            client="ca-pub-4882664754361963"
+                            slot="7450812669"
+                            style={{ display: 'block' }}
+                            layout="in-article"
+                            format="fluid"
+                        />
+                    </Footer>
+                </Layout>
+            </ConfigProvider>
             <Analytics />
         </>
     );
