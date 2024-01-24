17c929e8c33c04a4346f65aed2f30d609dd0df39
jameshu <james.hu@barecovetech.com>
2024-01-24 15:03:18 +0800

    feat: rework calculator with pictures

---
+++ .prettierrc.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+    semi: true,
+    tabWidth: 4,
+    useTabs: false,
+    bracketSpacing: true,
+    singleQuote: true,
+    trailingComma: 'all',
+    printWidth: 80,
+    proseWrap: 'never',
+    endOfLine: 'lf',
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,79 @@
-import { useEffect, useState } from "react";
+import {useEffect, useState} from "react";
 import "./App.css";
-import { Input, Select, Space } from "antd";
-import chinese from "./chinese.json";
-import result from "./result.json";
+import {Input, Select, Space} from "antd";
+import pals from "./pals.json";
+import matrix from "./matrix.json";
 
 function App() {
-  const [value1, setValue1] = useState(undefined);
-  const [value2, setValue2] = useState(undefined);
-  const [value3, setValue3] = useState(undefined);
-  useEffect(() => {
-    if (value1 && value2) {
-      const index = value1 + "," + value2;
-      // @ts-ignore
-      const res = result[index];
-      setValue3(res);
-    }
-  }, [value1, value2]);
-  return (
-    <>
-      <Space direction={"vertical"}>
-        <Space>
-          <Select
-            options={chinese}
-            style={{ width: 100 }}
-            onChange={(value) => {
-              setValue1(value);
-            }}
-          ></Select>
-          +
-          <Select
-            options={chinese}
-            style={{ width: 100 }}
-            onChange={(value) => {
-              setValue2(value);
-            }}
-          ></Select>
-        </Space>
-        =
-        <Input
-          style={{ backgroundColor: "#fff", color: "#000" }}
-          disabled={true}
-          value={value3 ? value3 : ""}
-        ></Input>
-      </Space>
-    </>
-  );
+    const [value1, setValue1] = useState("");
+    const [value2, setValue2] = useState("");
+    const [value3, setValue3] = useState("");
+    useEffect(() => {
+        if (value1 && value2) {
+            // @ts-ignore
+            const res = matrix[value1][value2];
+            setValue3(res);
+        }
+    }, [value1, value2]);
+
+    const filterOption = (
+        input: string,
+        option?: { label: string; value: string },
+    ) => (option?.label ?? "").toLowerCase().includes(input.toLowerCase());
+    return (
+        <>
+            <Space direction={"vertical"}>
+                <Space>
+                    <Select
+                        options={pals}
+                        showSearch={true}
+                        filterOption={filterOption}
+                        style={{width: 120}}
+                        onChange={(value) => {
+                            setValue1(value);
+                        }}
+                        optionRender={(option) => {
+                            const {label, data} = option;
+                            const {img} = data;
+                            return (
+
+                                <div className={"flex "}>
+                                    <img src={img} alt={"label"} className={"w-5 h-5"}/>
+                                    <span>{label}</span>
+                                </div>
+                            );
+                        }}
+                    ></Select>
+                    +
+                    <Select
+                        showSearch={true}
+                        filterOption={filterOption}
+                        options={pals}
+                        style={{width: 120}}
+                        onChange={(value) => {
+                            setValue2(value);
+                        }}
+                        optionRender={(option) => {
+                            const {label, data} = option;
+                            const {img} = data;
+                            return (
+                                <div className={"flex "}>
+                                    <img src={img} alt={"label"} className={"w-5 h-5"}/>
+                                    <span>{label}</span>
+                                </div>
+                            );
+                        }}
+                    ></Select>
+                </Space>
+                =
+                <Input
+                    style={{backgroundColor: "#fff", color: "#000"}}
+                    disabled={true}
+                    value={value3 ? pals.find((pal) => pal.value === value3)?.label : ""}
+                ></Input>
+            </Space>
+        </>
+    );
 }
 
 export default App;
